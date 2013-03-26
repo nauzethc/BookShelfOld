36d5280f45a1bff8c8cc452f75bdb2334b1f1194
Nauzet Hernandez <nauzet@horizon.tng.iac.es>
2013-03-26 17:27:58 +0000

    Added CSS and formatted controllers.js

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -2,55 +2,57 @@
 
 /* Controllers */
 
-function BookController($scope, $http, Book, User){
+function BookController($scope, $http, Book, User) {
     $scope.users = User.query();
     $scope.orderBooks = 'title';
     $scope.operationStatus = 'This is status';
 
     // Common operations
-    $scope.getBooks = function(){
+    $scope.getBooks = function() {
         $scope.books = Book.query();
     };
-    $scope.save = function(book){
-        Book.save({}, book, function(resource, status){
+    $scope.save = function(book) {
+        Book.save({}, book, function(resource, status) {
             // Clear form
             $scope.clearData();
             // Get new book from API and add it to list
             var headers = status();
-            $http.get(headers.location).success(function(book){
+            $http.get(headers.location).success(function(book) {
                 $scope.books.objects.push(book);
             });
         });
     };
-    $scope.remove = function(book){
+    $scope.remove = function(book) {
         var index = $scope.books.objects.indexOf(book);
-        Book.remove({}, book, function(){
+        Book.remove({}, book, function() {
             $scope.books.objects.splice(index, 1);
         });
     };
-    $scope.update = function(book){
+    $scope.update = function(book) {
         Book.update({}, book);
     };
 
     // Specific form operations
-    $scope.setOrder = function(order){
+    $scope.setOrder = function(order) {
         $scope.orderBooks = order;
     };
-    $scope.setTitle = function(book){
+    $scope.setTitle = function(book) {
         $scope.update(book);
         this.book.title = this.newTitle;
         this.disableEditor();
     };
 
     // Edit-in-place Title
-    $scope.enableEditor = function(){
+    $scope.enableEditor = function() {
         this.newTitle = this.book.title;
         this.showEditor = true;
     };
-    $scope.disableEditor = function(){
+    $scope.disableEditor = function() {
         this.showEditor = false;
     }
 
     // Reset form
-    $scope.clearData = function(){ $scope.newBook = null; };
+    $scope.clearData = function() {
+        $scope.newBook = null;
+    };
 }
